2598f34f21f669d980d392ea4f4c70d1bccc97ca
pasanyasaras <pasansmart@gmail.com>
2022-02-18 23:54:10 +0530

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/pasanyasara/projects/petclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package pasanyasara.projects.petclinic.services;
+
+import pasanyasara.projects.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
