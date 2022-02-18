fba78926d9835e3c3f94910c7ad2d6107e10c782
pasanyasaras <pasansmart@gmail.com>
2022-02-19 00:02:03 +0530

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/pasanyasara/projects/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/pasanyasara/projects/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package pasanyasara.projects.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/pasanyasara/projects/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/pasanyasara/projects/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package pasanyasara.projects.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
