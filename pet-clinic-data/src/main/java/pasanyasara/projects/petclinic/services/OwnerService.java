ff948be33c51314ec40680455a5fd12e30abf54b
pasanyasaras <pasansmart@gmail.com>
2022-02-24 23:33:45 +0530

    creating index page and controller. closes #8

--- pet-clinic-data/src/main/java/pasanyasara/projects/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/pasanyasara/projects/petclinic/services/OwnerService.java
@@ -4,7 +4,5 @@ import pasanyasara.projects.petclinic.model.Owner;
 
 public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-//    Owner findById(Long id);
-//    Owner save(Owner owner);
-//    Set<Owner> findAll();
+
 }
