60e6fe37147563b1769cf911994dcc39e9da63e3
pasanyasaras <pasansmart@gmail.com>
2022-02-24 20:43:33 +0530

    implementing map based services. closes #6

---
+++ pet-clinic-data/src/main/java/pasanyasara/projects/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package pasanyasara.projects.petclinic.services.map;
+
+import pasanyasara.projects.petclinic.model.Pet;
+import pasanyasara.projects.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet,Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
