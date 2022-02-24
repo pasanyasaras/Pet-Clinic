c6e858b70ad01e1badc14e877e471a394aaa3ebe
pasanyasaras <pasansmart@gmail.com>
2022-02-25 00:00:13 +0530

    adding vets page and controller. closes #7

---
+++ pet-clinic-web/src/main/java/pasanyasara/projects/petclinic/controllers/VetController.java
@@ -0,0 +1,14 @@
+package pasanyasara.projects.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets","/vets/index","/vets/vet_index.html"})
+    public String listVets()
+    {
+        return "vets/vet_index";
+    }
+}
