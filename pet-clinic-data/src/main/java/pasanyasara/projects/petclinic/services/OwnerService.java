099f199bd4d92b44ac13892c4d1d6c0d281cb62c
pasanyasaras <pasansmart@gmail.com>
2022-02-28 22:23:36 +0530

    adding owner list and update controller. closes #16

--- pet-clinic-data/src/main/java/pasanyasara/projects/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/pasanyasara/projects/petclinic/services/OwnerService.java
@@ -3,6 +3,7 @@ package pasanyasara.projects.petclinic.services;
 import pasanyasara.projects.petclinic.model.Owner;
 
 public interface OwnerService extends CrudService<Owner, Long> {
+
     Owner findByLastName(String lastName);
 
 }
--- pet-clinic-web/src/main/java/pasanyasara/projects/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pasanyasara/projects/petclinic/controllers/OwnerController.java
@@ -1,16 +1,28 @@
 package pasanyasara.projects.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pasanyasara.projects.petclinic.services.OwnerService;
 
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
     @RequestMapping({"/","/index","/owner_index.html",""})
-    public String listOwners()
+    public String listOwners(Model model)
     {
+        model.addAttribute("owners",ownerService.findAll());
+
         return "owners/owner_index";
     }
 }
